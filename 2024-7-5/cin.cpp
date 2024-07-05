c4f5aca452a812b757271764827bf28799e581d3
JasonWinterLXD <jason3120124635@petalmail.com>
2024-07-05 18:54:58 +0800

    cin

---
+++ 2024-7-5/cin.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // 1.整型
+    int a = 0;
+    cout << "请输入整型变量a：\n";
+    cin >> a;
+    cout << "整型变量a=" << a << endl;
+
+
+    return 0;
+}
