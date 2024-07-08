201fa3be04916411f76ca00824921dd57faf72a6
JasonWinterLXD <jason3120124635@petalmail.com>
2024-07-08 15:17:47 +0800

    while循环语句学习

---
+++ 7-8/whileXunHuanYuJu.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // 打印0~9
+
+    int a = 0;
+
+    while (a < 10)
+    {
+        cout << a << endl;
+        a++;
+    }
+
+    system("pause");
+
+    return 0;
+}
